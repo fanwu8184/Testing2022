158a1921662509df0c3cf4f775959cef338a0079
Fan Wu <fanwu8184@gmail.com>
2022-04-12 14:26:21 +1000

    Initial Commit

---
+++ Shared/Testing2022App.swift
@@ -0,0 +1,17 @@
+//
+//  Testing2022App.swift
+//  Shared
+//
+//  Created by Fan Wu on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Testing2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
