7c6882e7b637f3f07d94abb5226a4b58799298a1
Fan Wu <fanwu8184@gmail.com>
2022-10-06 09:40:58 +1100

    a new branch

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Button("Send Current deddddddd2d") {
+            Button("Send Current Time") {
                 bluetoothService.sendCurrentTime()
             }
             .padding()
